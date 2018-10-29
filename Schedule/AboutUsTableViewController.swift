ac66d11968469b68dc252b74a9e0e818827328b2
Yura Voevodin <voevodin.yura@icloud.com>
2018-10-29 21:07:07 +0200

    Added links to "Privacy Policy" and "Terms of Service".

--- Schedule/AboutUsTableViewController.swift
+++ Schedule/AboutUsTableViewController.swift
@@ -17,23 +17,39 @@ class AboutUsTableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        switch indexPath.row {
-        case 0:
-            if let facebookURL = URL(string: "fb://profile/120391101921477"), UIApplication.shared.canOpenURL(facebookURL) {
-                UIApplication.shared.open(facebookURL)
-            } else if let facebookPageURL = URL(string: "https://www.facebook.com/botschedule") {
-                UIApplication.shared.open(facebookPageURL)
+        if indexPath.section == 0 {
+            switch indexPath.row {
+            case 0:
+                if let facebookURL = URL(string: "fb://profile/120391101921477"), UIApplication.shared.canOpenURL(facebookURL) {
+                    UIApplication.shared.open(facebookURL)
+                } else if let facebookPageURL = URL(string: "https://www.facebook.com/botschedule") {
+                    UIApplication.shared.open(facebookPageURL)
+                }
+            case 1:
+                if let messengerBotURL = URL(string: "https://m.me/botschedule") {
+                    UIApplication.shared.open(messengerBotURL)
+                }
+            case 2:
+                if let telegramBotURL = URL(string: "https://telegram.me/sumdu_bot") {
+                    UIApplication.shared.open(telegramBotURL)
+                }
+            default:
+                break
             }
-        case 1:
-            if let messengerBotURL = URL(string: "https://m.me/botschedule") {
-                UIApplication.shared.open(messengerBotURL)
+        } else if indexPath.section == 1 {
+            switch indexPath.row {
+            case 0:
+                if let privacyPolicyURL = URL(string: "https://voevodin-yura.com/privacy-policy") {
+                    UIApplication.shared.open(privacyPolicyURL)
+                }
+            case 1:
+                if let termsOfServiceURL = URL(string: "https://voevodin-yura.com/terms-of-service") {
+                    UIApplication.shared.open(termsOfServiceURL)
+                }
+            default:
+                break
             }
-        case 2:
-            if let telegramBotURL = URL(string: "https://telegram.me/sumdu_bot") {
-                UIApplication.shared.open(telegramBotURL)
-            }
-        default:
-            break
         }
+        
     }
 }
