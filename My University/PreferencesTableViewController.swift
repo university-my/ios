3ecc780cbee3919c7baeb99082992d9a9504bb37
Yura Voevodin <voevodin.yura@icloud.com>
2019-10-24 22:14:20 +0300

    "Preferences" modal (not finished).

---
+++ My University/PreferencesTableViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PreferencesTableViewController.swift
+//  My University
+//
+//  Created by Yura Voevodin on 24.10.2019.
+//  Copyright © 2019 Yura Voevodin. All rights reserved.
+//
+
+import UIKit
+
+class PreferencesTableViewController: UITableViewController {
+
+    // MARK: - Done
+    
+    @IBAction func done(_ sender: Any) {
+        dismiss(animated: true)
+    }
+}
