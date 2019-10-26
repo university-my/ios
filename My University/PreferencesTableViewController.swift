6a784ec2e29bfe9c4efbb8f72c12cfa476e4bde2
Yura Voevodin <voevodin.yura@icloud.com>
2019-10-26 22:32:52 +0300

    Link to contacts from "Preferences" modal.

--- My University/PreferencesTableViewController.swift
+++ My University/PreferencesTableViewController.swift
@@ -15,4 +15,17 @@ class PreferencesTableViewController: UITableViewController {
     @IBAction func done(_ sender: Any) {
         dismiss(animated: true)
     }
+    
+    // MARK: - Report a problem
+    
+    @IBOutlet weak var reportProblemCell: UITableViewCell!
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let cell = tableView.cellForRow(at: indexPath)
+        if cell == reportProblemCell {
+            if let websiteURL = URL(string: "https://my-university.com.ua/contacts") {
+                UIApplication.shared.open(websiteURL)
+            }
+        }
+    }
 }
