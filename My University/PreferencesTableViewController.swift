4394afd82a3ad1ca695155c899a652d71f824319
Yura Voevodin <voevodin.yura@icloud.com>
2020-02-11 22:05:29 +0200

    Version 1.6.2, Build 26.

--- My University/PreferencesTableViewController.swift
+++ My University/PreferencesTableViewController.swift
@@ -10,6 +10,26 @@ import UIKit
 
 class PreferencesTableViewController: UITableViewController {
 
+    // MARK: - Navigation
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let identifier = segue.identifier else { return }
+
+        switch identifier {
+
+        case "privacyPolicy":
+            let vc = segue.destination as? LegalDocumentViewController
+            vc?.documentName = LegalDocument.privacyPolicy
+
+        case "termsOfService":
+            let vc = segue.destination as? LegalDocumentViewController
+            vc?.documentName = LegalDocument.termsOfService
+
+        default:
+            break
+        }
+    }
+
     // MARK: - Done
     
     @IBAction func done(_ sender: Any) {
