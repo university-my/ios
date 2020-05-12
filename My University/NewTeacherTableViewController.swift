87655793b98f91e18d2997d3c60c6bb072672ec0
Yura Voevodin <voevodin.yura@icloud.com>
2020-05-12 17:56:40 +0300

    New NewTeacherTableViewController

---
+++ My University/NewTeacherTableViewController.swift
@@ -0,0 +1,51 @@
+//
+//  NewTeacherTableViewController.swift
+//  My University
+//
+//  Created by Yura Voevodin on 12.05.2020.
+//  Copyright © 2020 Yura Voevodin. All rights reserved.
+//
+
+import UIKit
+
+final class NewTeacherTableViewController: EntityTableViewController {
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        dataController = TeacherTableDataController()
+        
+        super.viewDidLoad()
+    }
+    
+    // MARK: - Title
+    
+    @IBOutlet weak var tableTitleLabel: UILabel!
+    
+    // MARK: - Pull to refresh
+    
+    @IBAction func refresh(_ sender: Any) {
+        delegate?.didBeginRefresh(in: self)
+    }
+    
+    // MARK: - Navigation
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        switch segue.identifier {
+            
+        case "recordDetails":
+            if let navigation = segue.destination as? UINavigationController {
+                if let destination = navigation.viewControllers.first as? RecordDetailedTableViewController {
+                    destination.recordID = (sender as? Record)?.id
+                    destination.groupID = nil
+                    destination.teacherID = entityID
+                    destination.auditoriumID = nil
+                }
+            }
+            
+        default:
+            break
+        }
+    }
+}
