503cc4e75c6d906f9e90e70536e355268766dcba
Yura Voevodin <voevodin.yura@icloud.com>
2020-06-27 11:45:11 +0300

    Import MetricKit

--- My University/AppDelegate.swift
+++ My University/AppDelegate.swift
@@ -13,7 +13,7 @@ import MyLibrary
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
     
-
+    var metric: MetricKitSubscriber!
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -24,6 +24,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
         // Default settings for user
         UserData.registerDefaultValues()
+        
+        metric = MetricKitSubscriber()
 
         return true
     }
