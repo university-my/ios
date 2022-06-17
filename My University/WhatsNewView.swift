02f0366ea33877af892fe5dd228561047d4feb6b
Yura Voevodin <voevodin.yura@icloud.com>
2022-06-17 18:19:35 +0300

    Redesign "What's new" screen

--- My University/WhatsNewView.swift
+++ My University/WhatsNewView.swift
@@ -9,42 +9,42 @@
 import SwiftUI
 
 struct WhatsNewView: View {
-
+    
     var continueAction: (() -> Void)?
-
+    
     var body: some View {
-        VStack {
-            Spacer()
-            Image("Logo").resizable().frame(width: 120, height: 120, alignment: .center)
-
-            // Welcome text
-            VStack(spacing: 5.0) {
-                Text("Welcome").font(.largeTitle)
-                Text("Thank you for using this app! ❤️").font(.subheadline).fontWeight(.light)
-            }
-            
-            Spacer()
-
-            // What's new
-            VStack() {
-                Text("Check out what's new in this version:").font(.headline).padding()
+        ScrollView {
+            VStack {
+                Image("Logo")
+                    .resizable()
+                    .frame(width: 120, height: 120)
                 
-                VStack(alignment: .leading, spacing: 5) {
+                Text("Welcome")
+                    .font(.largeTitle)
+                    .padding(.vertical)
+                
+                Text("Thank you for using this app! ❤️")
+                    .font(.subheadline)
+                    .fontWeight(.light)
+                
+                Divider()
+                    .padding(.all)
+                
+                VStack() {
+                    Text("Check out what's new in this version:")
+                        .font(.headline)
+                        .padding(.vertical)
                     
-                    HStack(alignment: .center, spacing: 10) {
-                        Text("whats_new.universities_new_design").bold()
-                    }
-                    .frame(height: 50)
-
-                }.padding(.horizontal, 20.0)
-            }.padding(10)
-
-            Spacer()
-            
-            Button("Continue") { self.continueAction?() }
-                .padding(.bottom)
-                .buttonStyle(GradientButton())
+                    Text("whats_new.universities_new_design")
+                        .font(.body)
+                        .fontWeight(.light)
+                }
+                .padding(.horizontal)
+            }
         }
+        Button("Continue") { self.continueAction?() }
+            .padding(.vertical)
+            .buttonStyle(GradientButton())
     }
 }
 
